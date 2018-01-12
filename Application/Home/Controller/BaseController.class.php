6f9b8a4f0a82a14351350e9a6fe49e3c1fdc589d
tpp <13621323415@163.com>
2018-01-12 10:21:05 +0800

    机密  16 转10  10  转16

--- Application/Home/Controller/BaseController.class.php
+++ Application/Home/Controller/BaseController.class.php
@@ -12,4 +12,38 @@ class BaseController extends Controller {
         parent::__construct();
     	
     }
+    /*用于安全的url传输 
+     */
+    protected function strToHex($string)   
+    {   
+      $hex="";   
+      for   ($i=0;$i<strlen($string);$i++)   
+      $hex.=dechex(ord($string[$i]));   
+      $hex=strtoupper($hex);   
+      return   $hex;   
+   	}
+   
+   /**
+    * [hexToStr 将16进制转为字符串]
+    * @param  [type] $hex [十六进制]
+    * @return [type]      [字符串]
+    * 用于安全的url传输
+    */
+   	protected function hexToStr($hex)   
+   	{   
+      $string="";   
+      for   ($i=0;$i<strlen($hex)-1;$i+=2)   
+      $string.=chr(hexdec($hex[$i].$hex[$i+1]));   
+      return   $string;   
+    }
+
+    public function test()
+    {
+    	$a = '1/1/1/2';
+	    $b = strToHex($a);
+	    print_r($b);
+	    echo "\r\n";
+	    $c = hexToStr($b);
+	    print_r($c);
+    }
 }
